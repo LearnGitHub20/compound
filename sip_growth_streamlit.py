4424e24268a72c21583dbdcc8d07376baf202c9e
LearnGitHub20 <68774595+LearnGitHub20@users.noreply.github.com>
2025-10-19 17:35:25 +0100

    Update sip_growth_streamlit.py

--- sip_growth_streamlit.py
+++ sip_growth_streamlit.py
@@ -69,7 +69,8 @@ def build_yearly_df(total_years, results, increases, monthly_investment, rates, 
 # Streamlit UI
 # ----------------------------
 def main():
-	st.image("RFin logo.png", width=150)  # adjust width as needed
+	st.image("RFin logo.png", width=150) 
+	# adjust width as needed
     st.set_page_config(page_title="💷 Compound Interst Growth Simulator", layout="wide")
 
     # Add colorful CSS
@@ -173,3 +174,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
